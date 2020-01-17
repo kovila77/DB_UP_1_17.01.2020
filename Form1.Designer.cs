75824de58ca6c4eb875f999df09e3c2ee35c4b46
kovila77 <354981@mail.ru>
2020-01-17 22:44:56 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DB_UP_1_17_01_2020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvServers = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tvServers
+            // 
+            this.tvServers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvServers.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tvServers.Location = new System.Drawing.Point(0, 0);
+            this.tvServers.Name = "tvServers";
+            this.tvServers.Size = new System.Drawing.Size(800, 450);
+            this.tvServers.TabIndex = 0;
+            this.tvServers.AfterCollapse += new System.Windows.Forms.TreeViewEventHandler(this.tvServers_AfterCollapse);
+            this.tvServers.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.tvServers_AfterExpand);
+            this.tvServers.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvServers_NodeMouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tvServers);
+            this.Name = "Form1";
+            this.Text = "Server_tree";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvServers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_UP_1_17_01_2020
+{
+    public partial class Form1 : Form
+    {
+        //List<KeyValuePair< server_node, KeyValuePair< count_Child, coun_Child_Without_Child >>>
+        //private List<KeyValuePair<TreeNode, Tuple<int, int>>> servers;
+        Dictionary<TreeNode, ServerInfo> servers;
+        public Form1()
+        {
+            InitializeComponent();
+            //servers = new List<KeyValuePair<TreeNode, Tuple<int, int>>>();
+            servers = new Dictionary<TreeNode, ServerInfo>();
+            InitializeTVServers();
+        }
+
+        private void InitializeTVServers()
+        {
+            SQLiteConnectionStringBuilder sBuilder = new SQLiteConnectionStringBuilder();
+            sBuilder.DataSource = @"C:\Users\35498\Desktop\game_database.sqlite";
+            sBuilder.ForeignKeys = true;
+            string sConnStr = sBuilder.ConnectionString;
+            using (SQLiteConnection sConn = new SQLiteConnection(sConnStr))
+            {
+
+                sConn.Open();
+                SQLiteCommand sCommand = new SQLiteCommand();
+                sCommand.Connection = sConn;
+                sCommand.CommandText = @"
+                SELECT server.name as server_name,
+                       server.id as server_id,
+                       gamer.nickname as gamer_name,
+                       gamer.id as gamer_id,
+                       object.name as object_name
+                FROM server
+                    LEFT JOIN gamer ON server.id = gamer.server_id
+                    LEFT JOIN object ON gamer.id = object.owner_gamer_id
+                ORDER BY server_name,
+                         server.id,
+                         gamer_name,
+                         object_name,
+                         object.id
+                ";
+                using (SQLiteDataReader reader = sCommand.ExecuteReader())
+                {
+                    long lastServer = -1;
+                    long lastGamer = -1;
+                    TreeNode currentServerNode = null;
+                    TreeNode currentGamerNode = null;
+                    while (reader.Read())
+                    {
+                        string serverName = (string)reader["server_name"];
+                        long serverId = (long)reader["server_id"];
+                        var gamerName = reader["gamer_name"];
+                        var gamerId = reader["gamer_id"];
+                        var objectName = reader["object_name"];
+
+                        if (lastServer != serverId)
+                        {
+                            lastServer = serverId;
+                            currentServerNode = tvServers.Nodes.Add(serverName);
+                            servers.Add(currentServerNode, new ServerInfo(serverName));
+                        }
+
+                        if (gamerName != System.DBNull.Value)
+                        {
+                            if (lastGamer != (long)gamerId)
+                            {
+                                lastGamer = (long)gamerId;
+                                currentGamerNode = currentServerNode.Nodes.Add((string)gamerName);
+                                servers[currentServerNode].AddChl();
+                                if (objectName != System.DBNull.Value)
+                                {
+                                    servers[currentServerNode].AddChlWithChl();
+                                }
+                            }
+                            if (objectName != System.DBNull.Value)
+                            {
+                                currentGamerNode.Nodes.Add((string)objectName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private class ServerInfo
+        {
+            private int countChild;
+            private int countChildWithChild;
+
+            private string name;
+
+            public int CountChild { get { return countChild; } }
+            public int CountChildWithChild { get { return countChildWithChild; } }
+            public string Name { get { return name; } }
+
+            public ServerInfo(string name)
+            {
+                this.name = name;
+                this.countChild = 0;
+                this.countChildWithChild = 0;
+            }
+
+            public void AddChlWithChl()
+            {
+                countChildWithChild++;
+            }
+            public void AddChl()
+            {
+                countChild++;
+            }
+        }
+
+        private void tvServers_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+        }
+
+        private void tvServers_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Parent == null && servers.ContainsKey(e.Node))
+                e.Node.Text = $@"{e.Node.Text} - {servers[e.Node].CountChildWithChild}/{servers[e.Node].CountChild - servers[e.Node].CountChildWithChild}";
+
+
+        }
+
+        private void tvServers_AfterCollapse(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Parent == null && servers.ContainsKey(e.Node))
+                e.Node.Text = servers[e.Node].Name;
+        }
+    }
+}
